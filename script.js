a4aaa7bbff3356c8e2165863daf40a5626d44c27
Chris Behrens <chrisbehrens@yahoo.com>
2019-12-11 22:32:22 -0700

    made all questions HTML files and question JS files to fit on index.html and script.js pages. Add styles to css

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const startButton = document.getElementById("startQuiz");
 const timeEl = document.querySelector(".time");
+const falseEl1 = document.querySelectorAll(".false1")
+const trueEl1 = document.querySelector(".true1")
+const falseEl2 = document.querySelectorAll(".false2")
+const trueEl2= document.querySelector(".true2")
+const falseEl3 = document.querySelectorAll(".false3")
+const trueEl3= document.querySelector(".true3")
+const falseEl4 = document.querySelectorAll(".false4")
+const trueEl4= document.querySelector(".true4")
+const falseEl5 = document.querySelectorAll(".false5")
+const trueEl5= document.querySelector(".true5")
 
 let score = 0
 let questionsRight = 0
@@ -27,13 +37,102 @@ function setTime() {
     timeEl.textContent = "Time's up, no bonus points ";
   }
 
-  function nextPage() {
-    window.location = 'question1.html';
+  function hideMain() {
+    main.style.display = "none"
+    question1.style.display = "inline"
   }
 
 startButton.addEventListener("click", () => {
   setTime();
-  nextPage();
+  hideMain();
 
 })
 
+for (let i =0; i < falseEl1.length; i++){
+  falseEl1[i].addEventListener("click", function(nextq0){
+      nextq0.preventDefault();
+      question1.style.display = "none"
+      question2.style.display = "inline"
+    })
+}
+
+trueEl1.addEventListener("click", function(nextq3){
+  nextq3.preventDefault();
+  score = score + 20
+  questionsRight ++
+  localStorage.setItem ("right", questionsRight);
+  localStorage.setItem("score", score);
+  question1.style.display = "none"
+  question2.style.display = "inline"
+})
+
+for (let i =0; i < falseEl2.length; i++){
+  falseEl2[i].addEventListener("click", function(nextq0){
+      nextq0.preventDefault();
+      question2.style.display = "none"
+      question3.style.display = "inline"
+    })
+}
+
+trueEl2.addEventListener("click", function(nextq3){
+  nextq3.preventDefault();
+  score = score + 20
+  questionsRight ++
+  localStorage.setItem ("right", questionsRight);
+  localStorage.setItem("score", score);
+  question2.style.display = "none"
+  question3.style.display = "inline"
+})
+
+for (let i =0; i < falseEl3.length; i++){
+  falseEl3[i].addEventListener("click", function(nextq0){
+      nextq0.preventDefault();
+      question3.style.display = "none"
+      question4.style.display = "inline"
+    })
+}
+
+trueEl3.addEventListener("click", function(nextq3){
+  nextq3.preventDefault();
+  score = score + 20
+  questionsRight ++
+  localStorage.setItem ("right", questionsRight);
+  localStorage.setItem("score", score);
+  question3.style.display = "none"
+  question4.style.display = "inline"
+})
+
+for (let i =0; i < falseEl4.length; i++){
+  falseEl4[i].addEventListener("click", function(nextq0){
+      nextq0.preventDefault();
+      question4.style.display = "none"
+      question5.style.display = "inline"
+    })
+}
+
+trueEl4.addEventListener("click", function(nextq3){
+  nextq3.preventDefault();
+  score = score + 20
+  questionsRight ++
+  localStorage.setItem ("right", questionsRight);
+  localStorage.setItem("score", score);
+  question4.style.display = "none"
+  question5.style.display = "inline"
+})
+
+for (let i =0; i < falseEl5.length; i++){
+  falseEl5[i].addEventListener("click", function(nextq0){
+      nextq0.preventDefault();
+      question5.style.display = "none"
+      window.location = 'results.html';
+    })
+}
+
+trueEl5.addEventListener("click", function(nextq3){
+  nextq3.preventDefault();
+  score = score + 20
+  questionsRight ++
+  localStorage.setItem ("right", questionsRight);
+  localStorage.setItem("score", score);
+  window.location = 'results.html';
+})
